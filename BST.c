62f5b7601697b6f4e7c40f880e56bb5426033e16
ritik1312 <31184341+ritik1312@users.noreply.github.com>
2019-07-21 16:40:00 +0530

    Update BST.c

--- BST.c
+++ BST.c
@@ -1,4 +1,5 @@
-// To check whether tree is balanced or not
+//while inorder traversal it gives the next element or say successor of given element
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -27,44 +28,41 @@ void insert(struct node **root,int x)
 		insert(&((*root)->right),x);
 }
 
-int isSubtreeLesser(struct node *root,int x)		//for isBST() function
+void inorder_print(struct node *root)
 {
-	if(root==NULL)	return 1;
-	if(x>=root->val && isSubtreeLesser(root->left,x) && isSubtreeLesser(root->right,x))
-		return 1;
-	else
-		return 0;
-}
-int isSubtreeGreater(struct node *root,int x)		//for isBST() function
-{
-	if(root==NULL)	return 1;
-	if(x<=root->val && isSubtreeGreater(root->left,x) && isSubtreeGreater(root->right,x))
-		return 1;
-	else
-		return 0;
-}
-int isBST(struct node *root)
-{
-	if(root==NULL)	return 1;
-	if(isSubtreeLesser(root->left,root->val) && isSubtreeGreater(root->right,root->val)
-	 && isBST(root->left) && isBST(root->right))
-		return 1;
-	else
-		return 0;
+	if(root==NULL)
+		return;
+	inorder_print(root->left);
+	printf("%d ",root->val);
+	inorder_print(root->right);
 }
 
-int isBST_Util(struct node *root,int min,int max)	//for isBST_faster function only
+struct node search(struct node *root,int x)
 {
-	if(root==NULL)	return 1;
-	if(root->val>=min && root->val<=max 
-		&& isBST_Util(root->left,min,root->val) && isBST_Util(root->right,root->val,max))
-		return 1;
-	else
+	if(root==NULL)
 		return 0;
+	if(root->val==x)
+		return 1;
+	else if(x<=root->val)
+		return search(root->left,x);
+	else
+		return search(root->right,x);
 }
-int isBST_faster(struct node *root)
+struct node *find_Min(struct node *root)
 {
-	return isBST_Util(root,INT_MIN,INT_MAX);
+	while(root->left!=NULL)
+		root=root->left;
+	return root;
+}
+int inorder_successor(struct node *root,int x,int a)
+{
+	if(root==NULL)	return -1;
+	return inorder_successor(root->left,x,a);
+	if(a==x)
+		return root->val;
+	else
+		a=root->val;
+	return inorder_successor(root->right,x,a);
 }
 
 int main()
@@ -77,5 +75,7 @@ int main()
 		scanf("%d",&x);
 		insert(&root,x);
 	}
+	inorder_print(root);
+	printf("\n%d\n",inorder_successor(root,x,-1));
 	return 0;
 }
